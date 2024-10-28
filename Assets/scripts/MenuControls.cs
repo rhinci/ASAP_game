7dfbf18c93d19d76eb295146065b7c924425159c
rhinci <dashalupanova@gmail.com>
2024-10-28 18:45:04 +1000

    added menu

---
+++ Assets/scripts/MenuControls.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void PlayPressed()   //переключение сцены на игру
+    {
+        SceneManager.LoadScene("FirstScene");
+    }
+
+    public void ExitPressed() //выход из игры
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+}
