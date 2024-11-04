f34012f54e56bfde26e85ae6727619c74682f026
rhinci <dashalupanova@gmail.com>
2024-11-04 20:40:17 +1000

    death menu transition

--- Assets/scripts/PlayerScripts/Player.cs
+++ Assets/scripts/PlayerScripts/Player.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float health;
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            SceneManager.LoadScene("DeathMenu");
+        }
+    }
 }
