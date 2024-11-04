bfd938e3ad51d9a5a34da36855c725563280b885
rhinci <dashalupanova@gmail.com>
2024-11-04 18:14:58 +1000

    enemys attacks

--- Assets/scripts/EnemyScripts/enemy.cs
+++ Assets/scripts/EnemyScripts/enemy.cs
@@ -4,20 +4,69 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
     public int health;
     public float speed;
+    public int damage;
+    private float stopTime;
+    public float startStopTime;
+    public float normalSpeed;
 
-    void Update()
+    private Player player;
+    private Animator anim;
+
+    private void Start()
     {
+        anim = GetComponent<Animator>();
+        player = FindObjectOfType<Player>();
+        normalSpeed = speed;
+    }
+
+
+    private void Update()
+    {
+        if(stopTime <= 0)
+        {
+            speed = normalSpeed;
+        }
+        else
+        {
+            speed = 0;
+        }
         if(health <= 0)
         {
             Destroy(gameObject);
         }
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 
     public void TakeDamage(int damage)
     {
+        stopTime = startStopTime;
         health -= damage;
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(timeBtwAttack <= 0)
+            {
+                anim.SetTrigger("enemyAttack");
+            }
+            else
+            {
+                timeBtwAttack -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void OnEnemyAttack()
+    {
+        player.health -= damage;
+        timeBtwAttack = startTimeBtwAttack;
     }
 
 }
