4c58263ff5977b77618dc171645ce2b8f8651fa6
rhinci <dashalupanova@gmail.com>
2024-11-08 21:18:46 +1000

    5 locations and camera triggers

---
+++ Assets/scripts/OtherScripts/FadeUI.cs
@@ -0,0 +1,38 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    /*public GameObject Fade;
+    public float fadeSpeed = 1f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Image fadeImage = Fade.GetComponent<Image>();
+        Color color = fadeImage.color;
+        if (other.tag == "Player")
+        {
+            while(color.a < 1f)
+            {
+                color.a += fadeSpeed * Time.deltaTime;
+                fadeImage.color = color;
+
+
+            }
+        }
+    }
+    private void reFade()
+    {
+        while (color.a != 0f)
+        {
+            color.a += fadeSpeed * Time.deltaTime;
+            fadeImage.color = color;
+
+
+        }
+    }*/
+}
---
+++ Assets/scripts/OtherScripts/cameraTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using Unity.VisualScripting;
+
+public class cameraTrigger : MonoBehaviour
+{
+    public CinemachineVirtualCamera vcam;
+    public float LensSize;
+
+    private void Start()
+    {
+        //vcam = GetComponent<CinemachineVirtualCamera>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.tag == "Player")
+       {
+            vcam.GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = LensSize;
+       }
+    }
+
+}
