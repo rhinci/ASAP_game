0b5cb4deed3bedfe288ab14cb48a927b51232adb
rhinci <dashalupanova@gmail.com>
2024-11-10 00:37:00 +1000

    improved enemys behavior

--- Assets/scripts/EnemyScripts/enemy.cs
+++ Assets/scripts/EnemyScripts/enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class enemy : MonoBehaviour
@@ -10,21 +11,37 @@ public class enemy : MonoBehaviour
     public int health;
     public float speed;
     public int damage;
-    public float attackRange;
+    public int positionOfPatrol;
+    public Transform point;
+    public float stoppingDistance;
 
     public GameObject Player1, Player2;
     private GameObject ActivePlayer;
 
-    private Player player;
+    public GameObject player;
     private Animator anim;
     private Transform target;
     private bool facingLeft;
+
+    bool chill = false;
+    bool angry = false;
+    bool goBack = false;
 
     private void Start()
     {
         facingLeft = true;
         anim = GetComponent<Animator>();
-        player = FindObjectOfType<Player>();
+        switch (Player1.activeInHierarchy)
+        {
+            case true:
+                ActivePlayer = Player1;
+                break;
+            case false:
+                ActivePlayer = Player2;
+                break;
+
+
+        }
     }
 
 
@@ -41,38 +58,48 @@ public class enemy : MonoBehaviour
 
 
         }
+
+        if (Vector2.Distance(transform.position, point.position) < positionOfPatrol && angry == false)
+        {
+            chill = true;
+        }
+
+        if (Vector2.Distance(transform.position, ActivePlayer.GetComponent<Transform>().position) < stoppingDistance)
+        {
+            angry = true;
+            chill = false;
+            goBack = false;
+        }
+
+        if (Vector2.Distance(transform.position, ActivePlayer.GetComponent<Transform>().position) > stoppingDistance)
+        {
+            goBack = true;
+            angry = false;
+        }
+
+       
         target = ActivePlayer.GetComponent<Transform>();
+
         if (health <= 0)
         {
             Destroy(gameObject);
         }
-        //transform.Translate(Vector2.left * speed * Time.deltaTime);
 
-        // Вычисляем направление к игроку
-        Vector2 direction = target.position - transform.position;
-        Debug.Log(direction);
-
-        // Если игрок находится справа от врага
-        if (direction.x > 0 && facingLeft == true)
+        if (chill == true)
         {
-            // Отзеркаливаем врага
-            Flip();
-
+            Chill();
         }
-        else if (direction.x < 0 && facingLeft == false)
+        else if (angry == true)
         {
-            Flip();
+            Angry();
+        }
+        else if (goBack == true)
+        {
+            GoBack();
         }
     }
-    public void TakeDamage(int damage)
-    {
-        health -= damage;
-    }
 
-    private void FixedUpdate()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
-    }
+    
     public void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -88,9 +115,13 @@ public class enemy : MonoBehaviour
         }
     }
 
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
     public void OnEnemyAttack()
     {
-        player.health -= damage;
+        player.GetComponent<Player>().health -= damage;
         timeBtwAttack = startTimeBtwAttack;
     }
 
@@ -102,4 +133,47 @@ public class enemy : MonoBehaviour
         transform.localScale = Scaler;
     }
 
+    void Chill()
+    {
+        if (transform.position.x > point.position.x + positionOfPatrol)
+        {
+            Flip();
+        }
+        else if (transform.position.x < point.position.x - positionOfPatrol)
+        {
+            Flip();
+        }
+
+        if (facingLeft ==  false)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+
+    void Angry()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+
+        // Вычисляем направление к игроку
+        Vector2 direction = target.position - transform.position;
+        Debug.Log(direction);
+
+        // Если игрок находится справа от врага
+        if (direction.x > 0 && facingLeft == true)
+        {
+            Flip();
+        }
+        else if (direction.x < 0 && facingLeft == false)
+        {
+            Flip();
+        }
+    }
+    void GoBack()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.fixedDeltaTime);
+    }
 }
