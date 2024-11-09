426babeadb10598c01898894e269a22c873b3c66
rhinci <dashalupanova@gmail.com>
2024-11-09 21:27:50 +1000

    fixed enemy folowing and flip

--- Assets/scripts/EnemyScripts/enemy.cs
+++ Assets/scripts/EnemyScripts/enemy.cs
@@ -10,44 +10,69 @@ public class enemy : MonoBehaviour
     public int health;
     public float speed;
     public int damage;
-    private float stopTime;
-    public float startStopTime;
-    public float normalSpeed;
+    public float attackRange;
+
+    public GameObject Player1, Player2;
+    private GameObject ActivePlayer;
 
     private Player player;
     private Animator anim;
+    private Transform target;
+    private bool facingLeft;
 
     private void Start()
     {
+        facingLeft = true;
         anim = GetComponent<Animator>();
         player = FindObjectOfType<Player>();
-        normalSpeed = speed;
     }
 
 
     private void Update()
     {
-        if(stopTime <= 0)
+        switch (Player1.activeInHierarchy)
         {
-            speed = normalSpeed;
+            case true:
+                ActivePlayer = Player1;
+                break;
+            case false:
+                ActivePlayer = Player2;
+                break;
+
+
         }
-        else
-        {
-            speed = 0;
-        }
-        if(health <= 0)
+        target = ActivePlayer.GetComponent<Transform>();
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
-        transform.Translate(Vector2.left * speed * Time.deltaTime);
-    }
+        //transform.Translate(Vector2.left * speed * Time.deltaTime);
 
+        // Вычисляем направление к игроку
+        Vector2 direction = target.position - transform.position;
+        Debug.Log(direction);
+
+        // Если игрок находится справа от врага
+        if (direction.x > 0 && facingLeft == true)
+        {
+            // Отзеркаливаем врага
+            Flip();
+
+        }
+        else if (direction.x < 0 && facingLeft == false)
+        {
+            Flip();
+        }
+    }
     public void TakeDamage(int damage)
     {
-        stopTime = startStopTime;
         health -= damage;
     }
 
+    private void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+    }
     public void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
@@ -67,6 +92,14 @@ public class enemy : MonoBehaviour
     {
         player.health -= damage;
         timeBtwAttack = startTimeBtwAttack;
+    }
+
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 
 }
